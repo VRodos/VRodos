fd78d27eebc4c5274cb0a63a11d207cbbfdf8394
kostogls <38495639+kostogls@users.noreply.github.com>
2022-06-23 16:35:45 +0300

    update json and compile attributes to aframe

--- includes/vrodos-compile-aframe.php
+++ includes/vrodos-compile-aframe.php
@@ -7,6 +7,8 @@
 </head>
 <body>
 
+<!--simple json for test-->
+
 <!--{"box":[{"position":"-3 0.5 -3", "color":"red", "scale":"1 1 1", "rotation":"30 50 60"},
 {"position":"-5 0.5 -3", "color":"blue", "scale":"1 1 1", "rotation":"30 50 60"},
 {"position":"3 0.5 -2", "color":"cyan", "scale":"1 1 1", "rotation":"60 50 60"},
@@ -18,11 +20,140 @@
 $debug = 1;
 
 if ($debug) {
-            $sceneInput = '{"box":[{"position":"-3 0.5 -3", "color":"red", "scale":"1 1 1", "rotation":"30 50 60"},
-                            {"position":"-5 0.5 -3", "color":"blue", "scale":"1 1 1", "rotation":"30 50 60"},
-                            {"position":"3 0.5 -2", "color":"cyan", "scale":"1 1 1", "rotation":"60 50 60"},
-                            {"position":"-5 0.5 2","color":"green", "scale":"1 1 1", "rotation":"36 50 60"},
-                            {"position":"1 0.5 -3","color":"yellow", "scale":"1 1 1", "rotation":"30 50 60"}]}';
+            $sceneInput = '{
+  "metadata": {
+    "formatVersion" : 4.0,
+    "type"		: "scene",
+    "generatedBy"	: "SceneExporter.js",
+    "ClearColor" : "#eeeeee",
+    "toneMappingExposure" : "1",
+    "enableEnvironmentTexture" : "true",
+    "objects"       : 4	},
+
+  "urlBaseType": "relativeToScene",
+
+  "objects" :
+          {
+            "avatarYawObject" : {
+              "position" : [-1.5073529542515,1.3,3.7404941859495],
+              "rotation" : [0,-0.911,0],
+              "quaternion" : [0.0000,0.8980,0.0000,-0.4399],
+              "quaternion_player" : [0.0000,0.8980,0.0000,-0.4399],
+              "quaternion_camera" : [0.0000,0.0000,0.0000,1.0000],
+              "scale"	   : [1,1,1],
+              "categoryName" : "avatarYawObject",
+              "visible"  : true,
+              "children" : {
+              }
+            },
+
+            "torus-smooth_1655803767" : {
+              "position" : [-6.237191481778497,0,-5.212510024060256],
+              "rotation" : [0,0,0],
+              "quaternion" : [0,0,0,1],
+              "scale"	   : [1,1,1],
+              "fnPath" : "",
+              "assetid" : "995",
+              "assetname" : "Torus Smooth",
+              "fnObj" : "",
+              "fnObjID" : "",
+              "categoryName" : "Artifact",
+              "categoryDescription" : "undefined",
+              "categoryIcon" : "undefined",
+              "categoryID" : "41",
+              "fbxID" : "",
+              "glbID" : "996",
+              "color" : "2bffef",
+              "emissive" : "000000",
+              "roughness" : "0.12",
+              "metalness" : "0.86",
+              "emissiveIntensity" : "0",
+              "videoTextureSrc" : "",
+              "videoTextureRepeatX" : "",
+              "videoTextureRepeatY" : "",
+              "videoTextureCenterX" : "",
+              "videoTextureCenterY" : "",
+              "videoTextureRotation" : "",
+              "audioID" : "",
+              "image1id" : "",
+              "doorName_source" : "",
+              "doorName_target" : "",
+              "sceneName_target" : "",
+              "sceneID_target" : "",
+              "archaeology_penalty" : "0",
+              "hv_penalty" : "0",
+              "natural_penalty" : "0",
+              "isreward" : "0",
+              "isCloned" : "false",
+              "isLight" : "false",
+              "fnMtl" : "",
+              "fnMtlID" : "",
+              "children" : {
+              }
+            },
+
+            "sphere_1655803772" : {
+              "position" : [6.549051055867418,0,-5.791677804511449],
+              "rotation" : [0,0,0],
+              "quaternion" : [0,0,0,1],
+              "scale"	   : [1,1,1],
+              "fnPath" : "",
+              "assetid" : "449",
+              "assetname" : "Sphere",
+              "fnObj" : "",
+              "fnObjID" : "",
+              "categoryName" : "Artifact",
+              "categoryDescription" : "undefined",
+              "categoryIcon" : "undefined",
+              "categoryID" : "41",
+              "fbxID" : "",
+              "glbID" : "450",
+              "color" : "7f7f7f",
+              "emissive" : "000000",
+              "roughness" : "0.5",
+              "metalness" : "0",
+              "emissiveIntensity" : "0",
+              "videoTextureSrc" : "",
+              "videoTextureRepeatX" : "",
+              "videoTextureRepeatY" : "",
+              "videoTextureCenterX" : "",
+              "videoTextureCenterY" : "",
+              "videoTextureRotation" : "",
+              "audioID" : "",
+              "image1id" : "",
+              "doorName_source" : "",
+              "doorName_target" : "",
+              "sceneName_target" : "",
+              "sceneID_target" : "",
+              "archaeology_penalty" : "0",
+              "hv_penalty" : "0",
+              "natural_penalty" : "0",
+              "isreward" : "0",
+              "isCloned" : "false",
+              "isLight" : "false",
+              "fnMtl" : "",
+              "fnMtlID" : "",
+              "children" : {
+              }
+            },
+
+            "mylightSun_1655803780" : {
+              "position" : [0,13.453206671125372,16.799161901784828],
+              "rotation" : [0,0,0],
+              "quaternion" : [0,0,0,1],
+              "scale"	    : [1,1,1],
+              "lightintensity"	: "1",
+              "lightcolor"	: [1.000,1.000,1.000],
+              "targetposition" : [0,0,0],
+              "categoryName" : "lightSun",
+              "isLight"   : "true",
+              "children" : {
+              }
+            }
+
+          }
+
+}';
 } else {
         $scene_json = test_input($_GET["scene_json"]);
 }
@@ -48,7 +179,67 @@ function test_input($data) {
     const ascene = document.createElement("a-scene");
     document.body.appendChild(ascene);
 
-    // objects in scene
+    // iterate through the json_from_scene json (for the time we only need the objects item)
+
+    // array of glbs to load temporally
+    array_of_glbs = ["url(../assets/cubi.glb)","url(../assets/pawn.glb)"]
+    idx_of_glb = 0
+
+    // iterate through the dataobject json (for the time we only need the objects item)
+    for (var i=0; i<Object.keys(data_object.objects).length; i++) {
+        // get the values of objects
+        // check the type of asset in the scene
+
+        if (Object.values( data_object.objects)[i].categoryName==='Artifact'){
+            // console.log(Object.values( json_from_scene.objects)[i].color)
+            var glbs_fromJson = document.createElement("a-entity");
+
+            glbs_fromJson.setAttribute("gltf-model", array_of_glbs[idx_of_glb]);
+            glbs_fromJson.setAttribute("position", {x: Object.values( data_object.objects)[i].position[0],
+                y: Object.values( data_object.objects)[i].position[1],
+                z: Object.values( data_object.objects)[i].position[2]});
+
+            glbs_fromJson.setAttribute("rotation",{x: Object.values( data_object.objects)[i].rotation[0],
+                y: Object.values( data_object.objects)[i].rotation[1],
+                z: Object.values( data_object.objects)[i].rotation[2]});
+            glbs_fromJson.setAttribute("scale", {x: Object.values( data_object.objects)[i].scale[0],
+                y: Object.values( data_object.objects)[i].scale[1],
+                z: Object.values( data_object.objects)[i].scale[2]});
+            // glbs_fromJson.setAttribute('material', `color: ${Object.values( json_from_scene.objects)[i].color}`);
+            ascene.appendChild(glbs_fromJson);
+            idx_of_glb+=1;
+        }
+
+        // light seems ok
+        else if (Object.values( data_object.objects)[i].categoryName==='lightSun'){
+            // console.log(Object.values( json_from_scene.objects)[i].lightcolor)
+
+            sunlight = document.createElement('a-entity');
+            sunlight.setAttribute('light', {
+                color: Object.values( data_object.objects)[i].lightcolor,
+                intensity: Object.values( data_object.objects)[i].lightintensity
+            });
+            sunlight.setAttribute('position',{x: Object.values( data_object.objects)[i].position[0],
+                y: Object.values( data_object.objects)[i].position[1],
+                z: Object.values( data_object.objects)[i].position[2]});
+            sunlight.setAttribute('rotation',{x: Object.values( data_object.objects)[i].rotation[0],
+                y: Object.values( data_object.objects)[i].rotation[1],
+                z: Object.values( data_object.objects)[i].rotation[2]});
+            sunlight.setAttribute('scale',{x: Object.values( data_object.objects)[i].scale[0],
+                y: Object.values( data_object.objects)[i].scale[1],
+                z: Object.values( data_object.objects)[i].scale[2]});
+            ascene.appendChild(sunlight);
+
+        }
+        else if (Object.values( data_object.objects)[i].categoryName==='Pawn'){
+            pass;  }
+
+        // ascene.appendChild(glbs_fromJson);
+        // console.log("glbs", glbs_fromJson)
+
+    }
+	
+    // load separate objects in scene
     const abox = document.createElement("a-box");
     const acylinder = document.createElement("a-entity");
     const entityEl = document.createElement('a-entity');
@@ -65,39 +256,8 @@ function test_input($data) {
         intensity: 0.1
 
     });
-
-    directionalLight = document.createElement('a-entity');
-    directionalLight.setAttribute('light', {
-        color: '#FFF',
-        intensity: "0.5",
-        castShadow: true,
-    });
-
-    directionalLight.setAttribute('position', {x: -0.5, y: 1, z: 1});
-    // how to get the first item in the object (think it as we had multiple items, not only box)
-    console.log("check box: ", Object.keys(data_object)[0]);
-    // for loop to add all cubes
-    var first_item_box = Object.keys(data_object)[0]
-    for (var i=0; i<data_object.box.length; i++) {
-        // console.log(cubes_object.cubes[i]);
-        var abox2 = document.createElement("a-entity");
-
-        // abox2.setAttribute('geometry','primitive','box');
-        // abox2.setAttribute(('geometry', {primitive: 'box',
-        //     height: 3})
-        //     'material',{color: data_object.box[i].color});
-
-        abox2.setAttribute('geometry', 'primitive: box');
-        abox2.setAttribute('material', `color: ${data_object.box[i].color}`);
-        abox2.setAttribute("position", data_object.box[i].position)
-        abox2.setAttribute("scale", data_object.box[i].scale)
-        abox2.setAttribute("rotation", data_object.box[i].rotation)
-
-        ascene.appendChild(abox2);
-
-    }
-
-
+	
+    // load separate entities
     const ass_entity = document.createElement("a-entity");
     const ass_entity_sky = document.createElement("a-entity");
 
@@ -124,16 +284,14 @@ function test_input($data) {
 
     //appends to scene
 
-    // ascene.appendChild(abox);
-    // ascene.appendChild(acylinder);
     // ascene.appendChild(ass_entity);
     //ascene.appendChild(ass_entity_sky);
 
 
     // add lights to scene
 
-    ascene.appendChild(ambientLight);
-    ascene.appendChild(directionalLight);
+    // ascene.appendChild(ambientLight);
+    // ascene.appendChild(directionalLight);
 </script>
 
 
