9afcf765166856d7ec18d6de2808f3e5aa80de93
Dimitrios Ververidis <jimver04@gmail.com>
2022-08-25 11:10:30 +0300

    Fixed auxControllers. No bug now.

--- js_libs/vrodos_auxControlers.js
+++ js_libs/vrodos_auxControlers.js
@@ -12,8 +12,8 @@ var dg_s3_prev;
 // GUI controls
 /*var gui = new dat.GUI( {autoPlace: false} );*/
 
-
-var controlInterface = new dat.GUI( { autoPlace: false });
+var controlInterface = [];
+controlInterface = new dat.GUI( { autoPlace: false });
 controlInterface.domElement.style='width:100%';
 
 let coordLabel = ['<span style="color:red">X</span>', '<span style="color:green">Y</span>', '<span style="color:blue">Z</span>'];
@@ -22,16 +22,16 @@ let actionLabel = ['translate','translate','translate','rotate','rotate','rotate
 
 var dg_controller = Array();
 
-var gui_controls_funs = {
-    1 : 0,
-    2 : 0,
-    3 : 0,
-    4 : 0,
-    5: 0,
-    6 : 0,
-    7 : 0,
-    8 : 0,
-    9 : 0
+var gui_controls_funs = new function() {
+    this.dg_t1 = 0;
+    this.dg_t2 = 0;
+    this.dg_t3 = 0;
+    this.dg_r1 = 0;
+    this.dg_r2 = 0;
+    this.dg_r3 = 0;
+    this.dg_s1 = 0;
+    this.dg_s2 = 0;
+    this.dg_s3 = 0;
 };
 
 
@@ -55,97 +55,220 @@ for (let key in gui_controls_funs){
  */
 function controllerDatGuiOnChange() {
 
-    let actionLabel = ['position','position','position','rotation','rotation','rotation','scale', 'scale', 'scale'];
-    for (let k=0 ; k<9; k++){
-
-        dg_controller[k].onChange(function(value) {
+    let actionLabel = ['translate','translate','translate','rotate','rotate','rotate','scale', 'scale', 'scale'];
+    let k = 0;
+    for (let key in gui_controls_funs){
+        // When gui values changes then stop animating else won't be able to type with keyboard
+        dg_controller[0].onChange(function(value) {
 
                 // Stop animating
                 cancelAnimationFrame( id_animation_frame );
 
-                // update object position and scale
-                if (actionLabel[k] !== 'rotation')
-                {
-                    transform_controls.object[actionLabel[k]].setComponent(k % 3, value);
-                    // update rotation
-                } else if (actionLabel[k] === 'rotation') {
+                // update object position
+                transform_controls.object.position.x = gui_controls_funs.dg_t1;
 
-                    switch (k) {
-                        case 3:
-                            transform_controls.object['rotation'].x = value/180*Math.PI;
-                            break;
-                        case 4:
-                            transform_controls.object['rotation'].y = value/180*Math.PI;
-                            break;
-                        case 5:
-                            transform_controls.object['rotation'].z = value/180*Math.PI;
-                            break;
-                    }
-
-                }
                 // start animating again
                 animate();
             }
         );
-
     }
 
 
+    // When gui values changes then stop animating else won't be able to type with keyboard
+    dg_controller[0].onChange(function(value) {
 
+            // Stop animating
+            cancelAnimationFrame( id_animation_frame );
 
-    // When you move the mouse up or down inside a datgui then update object affines
-    for (let j=0; j<9; j++){
+            // update object position
+            transform_controls.object.position.x = gui_controls_funs.dg_t1;
 
-        // set an indicator for the operation
-        dg_controller[j].domElement.event3DOperation = j;
+            // start animating again
+            animate();
+        }
+    );
 
-        // Get the text field;
-        let element = dg_controller[j].domElement.childNodes[0];
+    dg_controller[1].onChange(function(value) {
+            cancelAnimationFrame( id_animation_frame );
+            transform_controls.object.position.y = gui_controls_funs.dg_t2;
+            animate();
+        }
+    );
 
-        // onclick inside stop animating
-        element.addEventListener("click", function (event) {
-            cancelAnimationFrame(id_animation_frame);
-        });
+    dg_controller[2].onChange(function(value) {
+            cancelAnimationFrame( id_animation_frame );
+            transform_controls.object.position.z = gui_controls_funs.dg_t3;
+            animate();
+        }
+    );
 
-        // While on Input Field on Focus and press enter
-        element.addEventListener('keydown', function (e) {
+    dg_controller[3].onChange(function(value) {
+            cancelAnimationFrame( id_animation_frame );
+            transform_controls.object.rotation.x = gui_controls_funs.dg_r1/180*Math.PI;
+            animate();
+        }
+    );
 
-            let operationIndex = element.parentElement.parentElement.event3DOperation;
+    dg_controller[4].onChange(function(value) {
+            cancelAnimationFrame( id_animation_frame );
+            transform_controls.object.rotation.y = gui_controls_funs.dg_r2 / 180*Math.PI;
+            animate();
+        }
+    );
 
-            // Update gui variables
-            gui_controls_funs[operationIndex] = element.value;
+    dg_controller[5].onChange(function(value) {
+            cancelAnimationFrame( id_animation_frame );
+            transform_controls.object.rotation.z = gui_controls_funs.dg_r3 / 180*Math.PI;
+            animate();
+        }
+    );
 
-            // Update transform controls
-            switch (operationIndex) {
-                case 0:
-                case 1:
-                case 2:
-                    transform_controls.object.position.setComponent(operationIndex, element.value);
-                    break;
-                case 3:
-                    transform_controls.object.rotation.x = element.value;
-                    break;
-                case 4:
-                    transform_controls.object.rotation.y = element.value;
-                    break;
-                case 5:
-                    transform_controls.object.rotation.z = element.value;
-                    break;
-                case 6:
-                case 7:
-                case 8:
-                    transform_controls.object.scale.setComponent(operationIndex,  element.value);
-                    break;
+    // When x length changes from dat gui then change also scale, y and z lengths, and scale the object with transform controls also
+    dg_controller[6].onChange( function(value) {
+
+            cancelAnimationFrame( id_animation_frame );
+
+            if (dg_s1_prev) {
+                transform_controls.object.scale.set(value, gui_controls_funs.dg_s2, gui_controls_funs.dg_s3);
+                envir.scene.dispatchEvent({type:"modificationPendingSave"});
             }
 
-            // 13 is enter
-            if (e.keyCode === 13) {
-                animate();
-                triggerAutoSave();
-            }
-        }, true);
+            dg_s1_prev = value;
+            animate();
+        }
+    );
 
-    }
+
+    dg_controller[7].onChange( function(value) {
+
+            cancelAnimationFrame( id_animation_frame );
+
+            if (dg_s2_prev) {
+                transform_controls.object.scale.set(gui_controls_funs.dg_s1, value, gui_controls_funs.dg_s3);
+                envir.scene.dispatchEvent({type:"modificationPendingSave"});
+            }
+
+            dg_s2_prev = value;
+            animate();
+        }
+    );
+
+
+    dg_controller[8].onChange( function(value) {
+
+            cancelAnimationFrame( id_animation_frame );
+
+            if (dg_s3_prev) {
+                //gui_controls_funs.dg_scale = gui_controls_funs.dg_scale * value / dg_s3_prev;
+
+                transform_controls.object.scale.set(gui_controls_funs.dg_s1, gui_controls_funs.dg_s2, value);
+
+                // var dims = findDimensions(transform_controls.object);
+                //
+                // gui_controls_funs.dg_s1 = dims[0];
+                // gui_controls_funs.dg_s2 = dims[1];
+                envir.scene.dispatchEvent({type:"modificationPendingSave"});
+            }
+
+            dg_s3_prev = value;
+            animate();
+        }
+    );
+
+    // Make slider-text controllers more interactive
+    dg_controller[0].domElement.event3DOperation = 'Tx';
+    setEventListenerKeyPressControllerConstrained(dg_controller[0].domElement.childNodes[0]);
+
+    dg_controller[1].domElement.event3DOperation = 'Ty';
+    setEventListenerKeyPressControllerConstrained(dg_controller[1].domElement.childNodes[0]);
+
+    dg_controller[2].domElement.event3DOperation = 'Tz';
+    setEventListenerKeyPressControllerConstrained(dg_controller[2].domElement.childNodes[0]);
+
+    dg_controller[3].domElement.event3DOperation = 'Rx';
+    setEventListenerKeyPressControllerConstrained(dg_controller[3].domElement.childNodes[0]);
+
+    dg_controller[4].domElement.event3DOperation = 'Ry';
+    setEventListenerKeyPressControllerConstrained(dg_controller[4].domElement.childNodes[0]);
+
+    dg_controller[5].domElement.event3DOperation = 'Rz';
+    setEventListenerKeyPressControllerConstrained(dg_controller[5].domElement.childNodes[0]);
+
+    dg_controller[6].domElement.event3DOperation = 'Sx';
+    setEventListenerKeyPressControllerConstrained(dg_controller[6].domElement.children[0]);
+
+    dg_controller[7].domElement.event3DOperation = 'Sy';
+    setEventListenerKeyPressControllerConstrained(dg_controller[7].domElement.childNodes[0]);
+
+    dg_controller[8].domElement.event3DOperation = 'Sz';
+    setEventListenerKeyPressControllerConstrained(dg_controller[8].domElement.childNodes[0]);
+}
+
+
+/**
+ * This function allows the dat gui text element of the slider to be clickable and interactive
+ * @param element
+ */
+function setEventListenerKeyPressControllerConstrained(element) {
+
+    element.addEventListener("focusout", function (event) {
+        animate();
+        triggerAutoSave();
+    });
+
+    // onclick inside stop animating
+    element.addEventListener("click", function (event) {
+        cancelAnimationFrame(id_animation_frame);
+    });
+
+    // While on Input Field on Focus and press enter
+    element.addEventListener('keydown', function (e) {
+
+        switch (element.parentElement.parentElement.event3DOperation) {
+            case 'Tx':
+                gui_controls_funs.dg_t1 = element.value;
+                transform_controls.object.position.x = element.value;
+                break;
+            case 'Ty':
+                gui_controls_funs.dg_t2 = element.value;
+                transform_controls.object.position.y = element.value;
+                break;
+            case 'Tz':
+                gui_controls_funs.dg_t3 = element.value;
+                transform_controls.object.position.z = element.value;
+                break;
+            case 'Sx':
+                gui_controls_funs.dg_s1 = element.value;
+                transform_controls.object.scale.x = element.value;
+                break;
+            case 'Sy':
+                gui_controls_funs.dg_s2 = element.value;
+                transform_controls.object.scale.y = element.value;
+                break;
+            case 'Sz':
+                gui_controls_funs.dg_s3 = element.value;
+                transform_controls.object.scale.z = element.value;
+                break;
+            case 'Rx':
+                gui_controls_funs.dg_r1 = element.value;
+                transform_controls.object.rotation.x = element.value;
+                break;
+            case 'Ry':
+                gui_controls_funs.dg_r2 = element.value;
+                transform_controls.object.rotation.y = element.value;
+                break;
+            case 'Rz':
+                gui_controls_funs.dg_r3 = element.value;
+                transform_controls.object.rotation.z = element.value;
+                break;
+        }
+
+        // 13 is enter
+        if (e.keyCode === 13) {
+            animate();
+            triggerAutoSave();
+        }
+    }, true);
 }
 
 
@@ -157,58 +280,64 @@ function controllerDatGuiOnChange() {
  */
 function updatePositionsPhpAndJavsFromControlsAxes(){
 
-
     //--------- translate_x ---------------
-    if (transform_controls.object.position.x !== gui_controls_funs[0]){
-        gui_controls_funs[0] = transform_controls.object.position.x;
+    if (transform_controls.object.position.x!= gui_controls_funs.dg_t1){
+        gui_controls_funs.dg_t1 = transform_controls.object.position.x;
+        // Auto-save
         envir.scene.dispatchEvent({type:"modificationPendingSave"});
     }
 
     //--------- translate_y ---------------
-    if (transform_controls.object.position.y !== gui_controls_funs[1]){
-        gui_controls_funs[1] = transform_controls.object.position.y;
+    if (transform_controls.object.position.y!= gui_controls_funs.dg_t2){
+        gui_controls_funs.dg_t2 = transform_controls.object.position.y;
+
+        // Auto-save
         envir.scene.dispatchEvent({type:"modificationPendingSave"});
     }
 
     //--------- translate_z ---------------
-    if (transform_controls.object.position.z !== gui_controls_funs[2]){
-        gui_controls_funs[2] = transform_controls.object.position.z;
+    if (transform_controls.object.position.z!= gui_controls_funs.dg_t3){
+        gui_controls_funs.dg_t3 = transform_controls.object.position.z;
+
+        // Auto-save
         envir.scene.dispatchEvent({type:"modificationPendingSave"});
     }
 
     //--------- rotate_x ----------------------
-    if (transform_controls.object.rotation._x*180/Math.PI !== gui_controls_funs[3]){
-        gui_controls_funs[3] = transform_controls.object.rotation._x * 180/Math.PI;
+    if (transform_controls.object.rotation._x*180/Math.PI != gui_controls_funs.dg_r1){
+        gui_controls_funs.dg_r1 = transform_controls.object.rotation._x * 180/Math.PI;
+
+        // Auto-save
         envir.scene.dispatchEvent({type:"modificationPendingSave"});
     }
 
     //---------rotate_y -------------------------------
-    if (transform_controls.object.rotation._y*180/Math.PI !== gui_controls_funs[4]){
-        gui_controls_funs[4] = transform_controls.object.rotation._y * 180/Math.PI;
+    if (transform_controls.object.rotation._y*180/Math.PI != this.dg_r2){
+        gui_controls_funs.dg_r2 = transform_controls.object.rotation._y * 180/Math.PI;
         envir.scene.dispatchEvent({type:"modificationPendingSave"});
     }
 
     //---------rotate_z -------------------------------
-    if (transform_controls.object.rotation._z*180/Math.PI !== gui_controls_funs[5]){
-        gui_controls_funs[5] = transform_controls.object.rotation._z * 180/Math.PI;
+    if (transform_controls.object.rotation._z*180/Math.PI != gui_controls_funs.dg_r3){
+        gui_controls_funs.dg_r3 = transform_controls.object.rotation._z * 180/Math.PI;
         envir.scene.dispatchEvent({type:"modificationPendingSave"});
     }
 
     //---------scale_x -------------------------------
-    if (transform_controls.object.scale.x !== gui_controls_funs[6]){
-        gui_controls_funs[6] = transform_controls.object.scale.x;
+    if (transform_controls.object.scale.x != gui_controls_funs.dg_s1){
+        gui_controls_funs.dg_s1 = transform_controls.object.scale.x;
         envir.scene.dispatchEvent({type:"modificationPendingSave"});
     }
 
     //---------scale_y -------------------------------
-    if (transform_controls.object.scale.y !== gui_controls_funs[7]){
-        gui_controls_funs[7] = transform_controls.object.scale.y;
+    if (transform_controls.object.scale.y != gui_controls_funs.dg_s2){
+        gui_controls_funs.dg_s2 = transform_controls.object.scale.y;
         envir.scene.dispatchEvent({type:"modificationPendingSave"});
     }
 
     //---------scale_z -------------------------------
-    if (transform_controls.object.scale.z !== gui_controls_funs[8]){
-        gui_controls_funs[8] = transform_controls.object.scale.z;
+    if (transform_controls.object.scale.z != gui_controls_funs.dg_s3){
+        gui_controls_funs.dg_s3 = transform_controls.object.scale.z;
         envir.scene.dispatchEvent({type:"modificationPendingSave"});
     }
 
