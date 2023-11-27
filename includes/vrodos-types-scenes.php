c96a486a5bdbe82ebbe72751008447cfc9edf0d2
Tasos Papazoglou Chalikias <tasos@tpapazoglou.com>
2023-11-27 13:08:48 +0200

    Deleted Help scene addition code.
    No longer used.

--- includes/vrodos-types-scenes.php
+++ includes/vrodos-types-scenes.php
@@ -33,29 +33,6 @@ $vrodos_scenes_metas_definition = array(
     )
 );
 
-$vrodos_scenes_metas_helpdata = array(
-    'id' => 'vrodos-scenes-databox-helpdata',
-    'page' => 'vrodos_scene',
-    'context' => 'side',
-    'priority' => 'low',
-    'fields' => array(
-        array(
-            'name' => 'Help Scene Text',
-            'desc' => 'Text for the Help scene (if activated)',
-            'id' => 'vrodos_scene_help_text',
-            'type' => 'textarea',
-            'std' => ''
-        ),
-        array(
-            'name' => 'Help Scene Image',
-            'desc' => 'Help Scene Image',
-            'id' => 'vrodos_scene_helpimg',
-            'type' => 'text',
-            'std' => ''
-        ),
-    )
-);
-
 
 // Create Scene - Scene as custom type 'vrodos_scene'
 function vrodos_scenes_construct() {
@@ -126,7 +103,7 @@ function vrodos_scenes_parent_project_tax_define() {
         'add_new_item' => __('Add New Parent Taxonomy'),
         'new_item_name' => __('New Parent Taxonomy')
     );
-    
+
     $args = array(
         'description' => 'Parent Taxonomy that the Scene belongs to',
         'labels' => $labels,
@@ -142,7 +119,7 @@ function vrodos_scenes_parent_project_tax_define() {
             'assign_terms' => 'edit_vrodos_taxpgame'
         ),
     );
-    
+
     register_taxonomy('vrodos_scene_pgame', 'vrodos_scene', $args);
 }
 
@@ -196,13 +173,13 @@ function vrodos_scenes_taxgame_box() {
 
 function vrodos_scenes_taxgame_box_content($post){
     $tax_name = 'vrodos_scene_pgame';
-    
+
     ?>
-    
+
     <div class="tagsdiv" id="<?php echo $tax_name; ?>">
-        
+
         <p class="howto"><?php echo 'Select Project for current Scene' ?></p>
-        
+
         <?php
         // Use nonce for verification
         wp_nonce_field( plugin_basename( __FILE__ ), 'vrodos_scene_pgame_noncename' );
@@ -220,32 +197,32 @@ function vrodos_scenes_taxgame_box_content($post){
             'option_none_value'  => '-1',
             'id' => 'vrodos-select-category-dropdown'
         );
-        
+
         $select = wp_dropdown_categories($args);
-        
+
         $replace = "<select$1 required>";
         $select  = preg_replace( '#<select([^>]*)>#', $replace, $select );
-        
+
         $old_option = "<option value='-1'>";
         $new_option = "<option disabled selected value=''>".'Select project'."</option>";
         $select = str_replace($old_option, $new_option, $select);
-        
+
         echo $select;
         ?>
-    
+
     </div>
     <?php
 }
 
 function vrodos_scenes_taxyaml_box_content($post){
     $tax_name = 'vrodos_scene_yaml';
-    
+
     ?>
-    
+
     <div class="tagsdiv" id="<?php echo $tax_name; ?>">
-        
+
         <p class="howto"><?php echo 'Select YAML for current Scene' ?></p>
-        
+
         <?php
         // Use nonce for verification
         wp_nonce_field( plugin_basename( __FILE__ ), 'vrodos_scene_yaml_noncename' );
@@ -263,19 +240,19 @@ function vrodos_scenes_taxyaml_box_content($post){
             'option_none_value'  => '-1',
             'id' => 'vrodos-select-category-dropdown'
         );
-        
+
         $select = wp_dropdown_categories($args);
-        
+
         $replace = "<select$1 required>";
         $select  = preg_replace( '#<select([^>]*)>#', $replace, $select );
-        
+
         $old_option = "<option value='-1'>";
         $new_option = "<option disabled selected value=''>".'Select YAML'."</option>";
         $select = str_replace($old_option, $new_option, $select);
-        
+
         echo $select;
         ?>
-    
+
     </div>
     <?php
 }
@@ -287,21 +264,21 @@ function vrodos_scenes_taxyaml_box_content($post){
 function vrodos_scenes_taxgame_box_content_save( $post_id ) {
 
     global $wpdb;
-    
+
     // verify if this is an auto save routine.
     // If it is our form has not been submitted, so we dont want to do anything
     if ( ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) || wp_is_post_revision( $post_id ) )
         return;
-    
+
     if (!isset($_POST['vrodos_scene_pgame_noncename']))
         return;
-    
+
     // verify this came from the our screen and with proper authorization,
     // because save_post can be triggered at other times
     if ( !wp_verify_nonce( $_POST['vrodos_scene_pgame_noncename'], plugin_basename( __FILE__ ) ) )
         return;
-    
-    
+
+
     // Check permissions
     if ( 'vrodos_scene' == $_POST['post_type'] )
     {
@@ -313,36 +290,36 @@ function vrodos_scenes_taxgame_box_content_save( $post_id ) {
         if ( ! ( current_user_can( 'edit_posts', $post_id ) ) )
             return;
     }
-    
+
     // OK, we're authenticated: we need to find and save the data
     $type_ID = intval($_POST['vrodos_scene_pgame'], 10);
-    
+
     $type = ( $type_ID > 0 ) ? get_term( $type_ID, 'vrodos_scene_pgame' )->slug : NULL;
-    
+
     wp_set_object_terms(  $post_id , $type, 'vrodos_scene_pgame' );
-    
+
 }
 
 
 
 function vrodos_scenes_taxyaml_box_content_save( $post_id ) {
-    
+
     global $wpdb;
-    
+
     // verify if this is an auto save routine.
     // If it is our form has not been submitted, so we dont want to do anything
     if ( ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) || wp_is_post_revision( $post_id ) )
         return;
-    
+
     if (!isset($_POST['vrodos_scene_yaml_noncename']))
         return;
-    
+
     // verify this came from the our screen and with proper authorization,
     // because save_post can be triggered at other times
     if ( !wp_verify_nonce( $_POST['vrodos_scene_yaml_noncename'], plugin_basename( __FILE__ ) ) )
         return;
-    
-    
+
+
     // Check permissions
     if ( 'vrodos_scene' == $_POST['post_type'] )
     {
@@ -354,14 +331,14 @@ function vrodos_scenes_taxyaml_box_content_save( $post_id ) {
         if ( ! ( current_user_can( 'edit_posts', $post_id ) ) )
             return;
     }
-    
+
     // OK, we're authenticated: we need to find and save the data
     $type_ID = intval($_POST['vrodos_scene_yaml'], 10);
-    
+
     $type = ( $type_ID > 0 ) ? get_term( $type_ID, 'vrodos_scene_yaml' )->slug : NULL;
-    
+
     wp_set_object_terms(  $post_id , $type, 'vrodos_scene_yaml' );
-    
+
 }
 
 function vrodos_set_custom_vrodos_scene_columns($columns) {
@@ -372,7 +349,7 @@ function vrodos_set_custom_vrodos_scene_columns($columns) {
 // Add the data to the custom columns for the scene post type
 function vrodos_set_custom_vrodos_scene_columns_fill( $column, $post_id ) {
     switch ( $column ) {
-        
+
         case 'scene_slug' :
             $mypost = get_post($post_id);
             $theSlug = $mypost->post_name;
@@ -387,47 +364,40 @@ function vrodos_set_custom_vrodos_scene_columns_fill( $column, $post_id ) {
 
 // Add and Show the metabox with Custom Field for Scene - ($vrodos_databox4)
 function vrodos_scenes_meta_definitions_add() {
-    global $vrodos_scenes_metas_definition, $vrodos_scenes_metas_helpdata, $post;
-    
-    
+    global $vrodos_scenes_metas_definition, $post;
+
+
     add_meta_box($vrodos_scenes_metas_definition['id'],
         'Scene Data',
         'vrodos_scenes_metas_adminside_show',
         $vrodos_scenes_metas_definition['page'],
         $vrodos_scenes_metas_definition['context'],
         $vrodos_scenes_metas_definition['priority']);
-    
-    add_meta_box($vrodos_scenes_metas_helpdata['id'],
-        'Help Scene Data',
-        'vrodos_scenes_metas_helpdata_adminside_show',
-        $vrodos_scenes_metas_helpdata['page'],
-        $vrodos_scenes_metas_helpdata['context'],
-        $vrodos_scenes_metas_helpdata['priority']);
 }
 
 
 // Scenes Databox Show
 function vrodos_scenes_metas_adminside_show(){
     global $vrodos_scenes_metas_definition, $post;
-    
+
     // Use nonce for verification
     echo '<input type="hidden" name="vrodos_scenes_databox_nonce" value="', wp_create_nonce(basename(__FILE__)), '" />';
-    
+
     //$categoryAsset = wp_get_post_terms($assetID, 'vrodos_asset3d_cat');
     //echo $categoryAssetSlug = $categoryAsset[0]->name;
-    
+
     echo '<table class="form-table" id="vrodos-custom-fields-table">';
-    
+
     foreach ($vrodos_scenes_metas_definition['fields'] as $field) {
-        
+
         // get current post meta data
         $meta = get_post_meta($post->ID, $field['id'], true);
         echo '<tr>',
         '<th style="width:20%"><label for="', esc_attr($field['id']), '">', esc_html($field['name']), '</label></th>',
         '<td>';
-        
-        
-        
+
+
+
         switch ($field['type']) {
             case 'text':
                 echo '<input type="text" name="', esc_attr($field['id']), '" id="', esc_attr($field['id']), '" value="', esc_attr($meta ? $meta : $field['std']), '" size="30" style="width:97%" />', '<br />', esc_html($field['desc']);
@@ -448,35 +418,22 @@ function vrodos_scenes_metas_adminside_show(){
             case 'checkbox':
                 echo '<input type="checkbox" name="', esc_attr($field['id']), '" id="', esc_attr($field['id']), '"', $meta ? ' checked="checked"' : '', ' />';
                 break;
-            
+
         }
         echo '</td><td>',
         '</td></tr>';
     }
     echo '</table>';
 }
-// Scene Meta help scene data show
-function vrodos_scenes_metas_helpdata_adminside_show(){
-    global $vrodos_scenes_metas_helpdata, $post;
-    // Use nonce for verification
-    echo '<input type="hidden" name="vrodos_scenes_databox_nonce" value="', wp_create_nonce(basename(__FILE__)), '" />';
-    
-    $value_of_helptext = get_post_meta($post->ID,$vrodos_scenes_metas_helpdata['fields'][0]['id'], true);
-    $value_of_helpimg = get_post_meta($post->ID,$vrodos_scenes_metas_helpdata['fields'][1]['id'], true);
-    echo '<textarea name="', esc_attr($vrodos_scenes_metas_helpdata['fields'][0]['id']), '" id="', esc_attr($vrodos_scenes_metas_helpdata['fields'][0]['id']), '" cols="60" rows="4" style="width:97%">', esc_attr($value_of_helptext ? $value_of_helptext : $vrodos_scenes_metas_helpdata['fields'][0]['std']), '</textarea>', '<br />', esc_html($vrodos_scenes_metas_helpdata['fields'][0]['desc']);
-    echo '<br /><br />';
-    echo '<input type="text" name="', esc_attr($vrodos_scenes_metas_helpdata['fields'][1]['id']), '" id="', esc_attr($vrodos_scenes_metas_helpdata['fields'][1]['id']), '" value="', esc_attr($value_of_helpimg ? $value_of_helpimg : $vrodos_scenes_metas_helpdata['fields'][1]['std']), '" size="30" style="width:97%" />', '<br />', esc_html($vrodos_scenes_metas_helpdata['fields'][1]['desc']);
-    //echo '<input id="' . esc_attr($vrodos_databox_helpdata['fields'][1]['id']) . '_btn" type="button" value="Upload Help Image"/>';
-}
 
 // Save data from this metabox with Custom Field for Scene
 function vrodos_scenes_metas_save($post_id) {
-    
-    global $vrodos_scenes_metas_definition,$vrodos_scenes_metas_helpdata;
-    
+
+    global $vrodos_scenes_metas_definition;
+
     if (!isset($_POST['vrodos_scenes_databox_nonce']))
         return;
-    
+
     // verify nonce
     if (!wp_verify_nonce($_POST['vrodos_scenes_databox_nonce'], basename(__FILE__))) {
         return $post_id;
@@ -502,17 +459,6 @@ function vrodos_scenes_metas_save($post_id) {
             delete_post_meta($post_id, $field['id'], $old);
         }
     }
-    foreach ($vrodos_scenes_metas_helpdata['fields'] as $field) {
-        $old = get_post_meta($post_id, $field['id'], true);
-        $new = $_POST[$field['id']];
-        if ($new && $new != $old) {
-            update_post_meta($post_id, $field['id'], $new);
-        } elseif ('' == $new && $old) {
-            delete_post_meta($post_id, $field['id'], $old);
-        }
-    }
 }
-
-
 
 ?>
