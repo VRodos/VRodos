e9cf30d41e204fa51a30df85ce9971c76d5e4169
Tasos Papazoglou Chalikias <tasos@tpapazoglou.com>
2025-12-05 19:28:11 +0200

    We dont need the networked aframe source files. Why do we even had them in the project in the first place?

--- networked-aframe/src/index.js
+++
@@ -1,9 +0,0 @@
-// Global vars and functions
-require('./NafIndex.js');
-
-// Network components
-require('./components/networked-scene');
-require('./components/networked');
-require('./components/networked-audio-source');
-require('./components/networked-video-source');
-require('./components/networked-hand-controls');
