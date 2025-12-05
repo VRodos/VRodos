322b573a6c54c9ea93bb50a70eb31de50661ed14
Tasos Papazoglou Chalikias <tasos@tpapazoglou.com>
2025-12-05 19:43:57 +0200

    Migrate output fils to new structure

--- networked-aframe/out/js/components/door_component.js
+++
@@ -1,9 +0,0 @@
-AFRAME.registerComponent('door-listener', {
-    schema: { type: "string", default: "default value" },
-    init: function () {
-        this.el.setAttribute("link", "on: click; href: " + this.data);
-        this.el.addEventListener("click", e => {
-            gtag('event', 'door_click');
-        });
-    }
-});
