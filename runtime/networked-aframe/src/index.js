87ab2bfcded4c5173593de2151d12eb75a0252c3
Tasos Papazoglou Chalikias <tasos@tpapazoglou.com>
2025-12-05 19:56:01 +0200

    Source files are needed for npm install NAF

---
+++ runtime/networked-aframe/src/index.js
@@ -0,0 +1,9 @@
+// Global vars and functions
+require('./NafIndex.js');
+
+// Network components
+require('./components/networked-scene');
+require('./components/networked');
+require('./components/networked-audio-source');
+require('./components/networked-video-source');
+require('./components/networked-hand-controls');
